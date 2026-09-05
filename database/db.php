3810496d467a9e0e7ffb681760e73fa7be0a9be5
ezrajnn <ezrajeant@gmail.com>
2026-09-05 16:40:27 +0800

    Fix database connection

--- database/db.php
+++ database/db.php
@@ -1,11 +1,11 @@
 <?php 
  
 $host = "localhost"; 
-$db   = "purr_and_pour"; 
+$db   = "purr-and-pour"; 
 $user = "root"; 
 $pass = ""; 
  
-$conn = new mysqli($host, $user, $pass, $db, 3304); 
+$conn = new mysqli($host, $user, $pass, $db, 3306); 
 if ($conn->connect_error) { 
     die("Connection failed: " . $conn->connect_error); 
 } else { 
